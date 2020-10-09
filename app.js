0d2eb1434e277e988549e62449ee305a5ae8bdfe
SaiPradeepti <69248387+SaiPradeepti@users.noreply.github.com>
2020-10-09 10:16:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//Init github
+const github = new Github;
+
+//Init UI
+const ui = new UI;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value;
+    if (userText !== '') {
+        github.getUser(userText)
+            .then(data => {
+                if (data.profile.message === 'Not Found') {
+                    //Show alert
+                    ui.showAlert('User not found', 'alert alert-danger');
+
+                } else {
+                    //show profile
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
+                }
+            });
+    }
+    else {
+        //clear profile
+        ui.clearProfile();
+    }
+});
